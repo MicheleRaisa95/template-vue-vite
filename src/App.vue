223b72464eaafc331efd36253111151f7d15dab9
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-03 16:48:59 +0200

    Vue Vite Template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+  <h1>template Vue Vite</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+
+// import style
+import './assets/scss/main.scss'
+
+// import vue root component
+import App from './App.vue'
+
+createApp(App).mount('#app')
